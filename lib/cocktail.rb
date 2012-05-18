9d72712faeb61b4c0df07f26161a18e0ec21f640
mcasimir <maurizio.cas@gmail.com>
2012-05-18 15:18:57 +0200

    Removed unnecessary dependencies and raise unless ruby >= 1.9

--- lib/cocktail.rb
+++ lib/cocktail.rb
@@ -1,19 +1,16 @@
+if RUBY_VERSION < "1.9"
+  raise "Sorry, Cocktail requires ruby >= 1.9"
+end
+
 require "cocktail/version"
 require "cocktail/mixable"
 require "cocktail/target"
 
-# eg.
-# 
-# class PostsController < Controller
-#   mixin(MyScaffold, :resource => Post, :only => :show)
-#   
-# end
-
 module Cocktail
-
-
-
 end
 
 Class.send :include,  Cocktail::Target
 Object.send :include, Cocktail::Mixable
+
+
+
--- lib/cocktail/version.rb
+++ lib/cocktail/version.rb
@@ -1,3 +1,3 @@
 module Cocktail
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
